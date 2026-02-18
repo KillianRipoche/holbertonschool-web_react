9d8e56dcaab29f9e3fbf050ee45b3174d1a4f948
KillianRipoche <killian.ripoche@gmail.com>
2026-02-18 10:11:07 +0100

    Initial commit

---
+++ react_styling/task_0/dashboard/src/Notifications/NotificationItem.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class NotificationItem extends React.PureComponent {
+  render() {
+    const { type, html, value, markAsRead, id } = this.props;
+    const style = { color: type === 'urgent' ? 'red' : 'blue' };
+
+    if (html) {
+      return (
+        <li
+          data-notification-type={type}
+          style={style}
+          dangerouslySetInnerHTML={html}
+          onClick={() => markAsRead(id)}
+        />
+      );
+    }
+    return (
+      <li
+        data-notification-type={type}
+        style={style}
+        onClick={() => markAsRead(id)}
+      >
+        {value}
+      </li>
+    );
+  }
+}
+
+NotificationItem.propTypes = {
+  type: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  markAsRead: PropTypes.func,
+  id: PropTypes.number,
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  markAsRead: () => {},
+};
+
+export default NotificationItem;
