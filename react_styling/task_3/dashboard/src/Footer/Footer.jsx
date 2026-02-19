da4e8cccc531f546ec867565f8e722d364a7dc6c
KillianRipoche <killian.ripoche@gmail.com>
2026-02-19 15:06:22 +0100

    Modifier le composant Footer pour le rendre non fixe et ajuster l'état de connexion dans le composant App

--- react_styling/task_3/dashboard/src/Footer/Footer.jsx
+++ react_styling/task_3/dashboard/src/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { getCurrentYear, getFooterCopy } from '../utils/utils';
 
 function Footer() {
   return (
-    <div className="App-footer fixed bottom-0 left-0 w-full border-t-[3px] border-main-color p-5 text-center italic bg-white">
+    <div className="App-footer w-full border-t-[3px] border-main-color p-5 text-center italic bg-white mt-auto">
       <p>Copyright {getCurrentYear()} - {getFooterCopy(false)}</p>
     </div>
   );
