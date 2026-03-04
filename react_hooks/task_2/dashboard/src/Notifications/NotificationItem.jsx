37da75c9f1434ae3bcc2df50b651dc4cd83d1dfb
KillianRipoche <killian.ripoche@gmail.com>
2026-03-04 11:05:17 +0100

    Refactor NotificationItem component to use memo and functional component syntax

--- react_hooks/task_2/dashboard/src/Notifications/NotificationItem.jsx
+++ react_hooks/task_2/dashboard/src/Notifications/NotificationItem.jsx
@@ -1,52 +1,50 @@
-import { PureComponent } from 'react'
+import { memo } from 'react'
 import { getLatestNotification } from '../utils/utils'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
-class NotificationItem extends PureComponent {
-  static defaultProps = {
-    markAsRead: () => { },
-    type: "default",
-    html: "",
-    value: "",
-    id: 1
+const NotificationItem = memo(function NotificationItem({
+  markAsRead,
+  type = "default",
+  html,
+  value,
+  id = 1
+}) {
+  const innerHtml = { __html: getLatestNotification() }
+  const mobileClasses = "max-[912px]:text-xl max-[912px]:border-b max-[912px]:border-black max-[912px]:py-2.5 max-[912px]:px-0"
+
+  if (type === "default") {
+    return (
+      <li
+        onClick={() => markAsRead(id)}
+        data-notification-type={type}
+        className={`text-[color:var(--default-notification-item)] pl-1 ${mobileClasses}`}
+      >
+        {value}
+      </li>
+    )
+  } else if (type === "urgent" && html) {
+    return (
+      <li
+        onClick={() => markAsRead(id)}
+        data-notification-type={type}
+        dangerouslySetInnerHTML={innerHtml}
+        className={`text-[color:var(--urgent-notification-item)] pl-1 ${mobileClasses}`}
+      />
+    )
+  } else if (type === "urgent") {
+    return (
+      <li
+        onClick={() => markAsRead(id)}
+        data-notification-type={type}
+        className={`text-[color:var(--urgent-notification-item)] pl-1 ${mobileClasses}`}
+      >
+        {value}
+      </li>
+    )
   }
 
-  render() {
-    const { markAsRead, type, html, value, id } = this.props
-    const innerHtml = { __html: getLatestNotification() }
-    const mobileClasses = "max-[912px]:text-xl max-[912px]:border-b max-[912px]:border-black max-[912px]:py-2.5 max-[912px]:px-0"
-
-    if (type === "default")
-      return (
-        <li onClick={() => markAsRead(id)}
-          data-notification-type={type}
-          className={`text-[color:var(--default-notification-item)] pl-1 ${mobileClasses}`}>
-          {value}
-        </li>
-      )
-    else if (type === "urgent" && html) {
-      return (
-        <li
-          onClick={() => markAsRead(id)}
-          data-notification-type={type}
-          dangerouslySetInnerHTML={innerHtml}
-          className={`text-[color:var(--urgent-notification-item)] pl-1 ${mobileClasses}`}>
-        </li>
-      )
-    }
-    else if (type === "urgent") {
-      return (
-        <li
-          onClick={() => markAsRead(id)}
-          data-notification-type={type}
-          className={`text-[color:var(--urgent-notification-item)] pl-1 ${mobileClasses}`}
-        >
-          {value}
-        </li>
-      )
-    }
-  }
-}
+  return null
+})
 
 NotificationItem.propTypes = {
   markAsRead: PropTypes.func,
@@ -55,5 +53,15 @@ NotificationItem.propTypes = {
   value: PropTypes.string,
   id: PropTypes.number,
 }
+
+NotificationItem.defaultProps = {
+  markAsRead: () => {},
+  type: "default",
+  html: undefined,
+  value: "",
+  id: 1,
+}
+
+NotificationItem.displayName = 'NotificationItem'
 
 export default NotificationItem
