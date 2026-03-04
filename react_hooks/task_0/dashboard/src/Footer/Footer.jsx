888a7490fe13e50607db7ce57d6c9f9736c7907f
KillianRipoche <killian.ripoche@gmail.com>
2026-03-04 10:45:46 +0100

    Refactor Footer component to use useContext for user state management

--- react_hooks/task_0/dashboard/src/Footer/Footer.jsx
+++ react_hooks/task_0/dashboard/src/Footer/Footer.jsx
@@ -1,28 +1,25 @@
-import React from 'react'
+import { useContext } from 'react'
 import { getCurrentYear, getFooterCopy } from '../utils/utils'
 import AppContext from '../Context/context'
 
 function Footer({ isIndex = false }) {
   const currentYear = getCurrentYear()
   const footerCopy = getFooterCopy(isIndex)
+  const { user } = useContext(AppContext)
 
   return (
-    <AppContext.Consumer>
-      {({ user }) => (
-        <>
-          <div className="App-footer flex justify-center items-center border-t-4 border-[color:var(--main-color)] bottom-0 mt-auto">
-            <p className="italic text-xl p-1 max-[912px]:text-base">
-              Copyright {currentYear} - {footerCopy}
-            </p>
-          </div>
-          {user.isLoggedIn && (
-            <p className="text-center">
-              <a href="#" className="text-blue-600 underline">Contact us</a>
-            </p>
-          )}
-        </>
+    <>
+      <div className="App-footer flex justify-center items-center border-t-4 border-[color:var(--main-color)] bottom-0 mt-auto">
+        <p className="italic text-xl p-1 max-[912px]:text-base">
+          Copyright {currentYear} - {footerCopy}
+        </p>
+      </div>
+      {user.isLoggedIn && (
+        <p className="text-center">
+          <a href="#" className="text-blue-600 underline">Contact us</a>
+        </p>
       )}
-    </AppContext.Consumer>
+    </>
   )
 }
 
